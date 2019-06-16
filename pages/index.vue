d814eac32221bf44126aad60e5a597d58630ab51
Vannsl <boehner.vanessa@gmail.com>
2019-06-16 09:15:27 +0200

    basic layout

--- pages/index.vue
+++ pages/index.vue
@@ -8,18 +8,6 @@
       <h2 class="subtitle">
         Workshop: Nuxt.js - VueJS SSR
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
     </div>
   </div>
 </template>
@@ -40,14 +28,6 @@ export default {
   @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
