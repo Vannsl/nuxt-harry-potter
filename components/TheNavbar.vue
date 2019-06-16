7140353220a4cfceb20c086039cbb9c8090e3732
Vannsl <boehner.vanessa@gmail.com>
2019-06-16 09:38:39 +0200

    adds and imports navbar to default layout

---
+++ components/TheNavbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex items-center bg-white">
+    <nuxt-link
+      to="/"
+      class="logo bg-cover block"
+    />
+    <nuxt-link to="/characters">
+      Characters
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheNavbar'
+}
+</script>
+
+<style scoped>
+.logo {
+  background-image: url('~assets/img/logo-navbar.png');
+  height: 80px;
+  width: 80px;
+}
+
+a:hover {
+  color: goldenrod;
+}
+
+.nuxt-link-exact-active {
+  font-weight: bold;
+}
+</style>
