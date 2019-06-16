a6eeb74d94ca773f239b256a1ff0df831eb665fe
Vannsl <boehner.vanessa@gmail.com>
2019-06-16 09:35:46 +0200

    creates page characters

---
+++ pages/characters.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class="text-5xl">
+      Harry Potter Characters
+    </h1>
+    <p>
+      On this page I will show all Harry Potter characters!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Characters'
+}
+</script>
