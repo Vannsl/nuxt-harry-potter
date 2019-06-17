9917c27c159dd5a2a540ae420e0fca63e088a5fe
Vannsl <boehner.vanessa@gmail.com>
2019-06-17 16:16:04 +0200

    fetches data using fetch in mounted hook as promise

--- pages/characters.vue
+++ pages/characters.vue
@@ -11,6 +11,19 @@
 
 <script>
 export default {
-  name: 'Characters'
+  name: 'Characters',
+  data() {
+    return {
+      characters: []
+    }
+  },
+  mounted() {
+    fetch('https://hp-api.herokuapp.com/api/characters')
+      .then((response) => {
+        response.json().then((characters) => {
+          this.characters = characters
+        })
+      })
+  }
 }
 </script>
