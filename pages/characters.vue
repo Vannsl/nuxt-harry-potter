f72376ed87b35844b19f54d6ab4685dc2a65a2cb
Vannsl <boehner.vanessa@gmail.com>
2019-06-17 16:20:08 +0200

    adds character component

--- pages/characters.vue
+++ pages/characters.vue
@@ -3,15 +3,27 @@
     <h1 class="text-5xl">
       Harry Potter Characters
     </h1>
-    <p>
-      On this page I will show all Harry Potter characters!
-    </p>
+    <div class="flex flex-wrap justify-around">
+      <character
+        v-for="character in characters"
+        :key="character.name"
+        :name="character.name"
+        :image="character.image"
+        :house="character.house"
+        class="w-5/12 mx-1 my-2"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import Character from '@/components/character'
+
 export default {
   name: 'Characters',
+  components: {
+    character: Character
+  },
   data() {
     return {
       characters: []
