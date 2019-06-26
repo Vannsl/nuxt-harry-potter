bc7f28056f18058cf8d67fe7109c67d661782835
Vannsl <boehner.vanessa@gmail.com>
2019-06-26 20:18:38 +0200

    adds meta data for index

--- pages/index.vue
+++ pages/index.vue
@@ -19,6 +19,27 @@ export default {
   components: {
     Logo
   },
+  head() {
+    return {
+      meta: [
+        {
+          name: 'twitter:card', content: 'summary'
+        },
+        {
+          name: 'twitter:site', content: '@Vannsl'
+        },
+        {
+          name: 'twitter:image', content: 'https://www.towa-digital.com/wp-content/uploads/2017/01/team__towa_digitalagentur-1920x1080.jpg'
+        },
+        {
+          name: 'twitter:title', content: 'Nuxt.js Workshop @ TOWA (Bregenz)'
+        },
+        {
+          name: 'twitter:description', content: 'RIGHT NOW we are creating a nuxt.js application which renders aaaall of the amazing Harry Potter characters.'
+        }
+      ]
+    }
+  },
   layout: 'light'
 }
 </script>
